febf9c614c6810e5b6c497497f41584764882a0d
MikhailKuznets <k-u-z-n-e-c@yandex.ru>
2022-10-31 21:57:32 +0300

    fix: исправить все замечания первого ревью

--- src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryFilmStorage.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate.storage.inmemory;
 
-import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.AbstractStorage;
-import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
-//public class InMemoryFilmStorage extends AbstractStorage<Film> implements FilmStorage {
-public class InMemoryFilmStorage  {
+public class InMemoryFilmStorage extends AbstractStorage<Film> {
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryUserStorage.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate.storage.inmemory;
 
-import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.AbstractStorage;
-import ru.yandex.practicum.filmorate.storage.UserStorage;
 
-//public class InMemoryUserStorage extends AbstractStorage<User> implements UserStorage {
-public class InMemoryUserStorage {
+public class InMemoryUserStorage extends AbstractStorage<User>{
+
 }
